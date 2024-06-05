d6aa38fff00d9457e4160ae3f42165409ef99b34
dalramjwi <dalramjwi@gmail.com>
2024-06-05 11:52:19 +0900

    api 주소 분석

---
+++ item.js
@@ -0,0 +1,10 @@
+const endpoint = `http://apis.data.go.kr/6300000/eventDataService`;
+const ex = `http://apis.data.go.kr/6300000/
+eventDataService/
+eventDataListJson
+?
+serviceKey=인증키(URL Encode)
+&
+numOfRows=10
+&
+pageNo=1`;

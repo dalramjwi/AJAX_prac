d7c1786852d57932442d194f0a6ce49155a33b92
dalramjwi <dalramjwi@gmail.com>
2024-06-05 11:55:25 +0900

    인증키 리터럴로 삽입

--- item.js
+++ item.js
@@ -1,9 +1,10 @@
 const endpoint = `http://apis.data.go.kr/6300000/eventDataService`;
+const keyname = `NF%2Fq2Qd4j83X%2F%2BGBIgSXa02sRgXzX8PKbbn15PVpwZE16Bqu9uaot%2FvnTGhtq808yAE2RDj2LNxM7tkieD%2FU9g%3D%3D`;
 const ex = `http://apis.data.go.kr/6300000/
 eventDataService/
 eventDataListJson
 ?
-serviceKey=인증키(URL Encode)
+serviceKey=${keyname}
 &
 numOfRows=10
 &

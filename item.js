2e3c9fca423ebd6ab93a7c0680137e2963d828b5
dalramjwi <dalramjwi@gmail.com>
2024-06-05 12:05:03 +0900

    AJAX 사용 시작

--- item.js
+++ item.js
@@ -1,11 +1,5 @@
 const endpoint = `http://apis.data.go.kr/6300000/eventDataService`;
 const keyname = `NF%2Fq2Qd4j83X%2F%2BGBIgSXa02sRgXzX8PKbbn15PVpwZE16Bqu9uaot%2FvnTGhtq808yAE2RDj2LNxM7tkieD%2FU9g%3D%3D`;
-const ex = `http://apis.data.go.kr/6300000/
-eventDataService/
-eventDataListJson
-?
-serviceKey=${keyname}
-&
-numOfRows=10
-&
-pageNo=1`;
+const ex = `http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=${keyname}&numOfRows=10&pageNo=1`;
+
+console.log(ex);
